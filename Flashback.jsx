a5cf3d4a8dbc878fa5f4dbe2ff43071ceee60584
Jadsia <megankyoung@live.com>
2019-11-08 22:17:19 -0800

    added scss file and initial message

--- Flashback.jsx
+++ Flashback.jsx
@@ -1,12 +1,14 @@
 <html>
-      <head>
-        <title>Flashback</title>
-            <script src="https://unpkg.com/@babel/standalone@7.2.5/babel.min.jss"></script>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/react/16.0.0/umd/react.development.js"></script>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/react-dom/16.0.0/umd/react-dom.development.js"></script>
-      </head>
+  <head>
+    <title>Flashback</title>
+    <script src="https://unpkg.com/@babel/standalone@7.2.5/babel.min.jss"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/react/16.0.0/umd/react.development.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/react-dom/16.0.0/umd/react-dom.development.js"></script>
+    <link rel="stylesheet" type="text/css" href="index.css"></link>
+  </head>
 
-    <body>
-    </body>
+  <body>
+    <h1>What it do, bitch?</h1>
 
-  </html>
+  </body>
+</html>
